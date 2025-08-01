0dc1007bc03410b0373237bb8d80d0db5ad5e5c3
Daniel Zabala <danielzabalanava@gmail.com>
2025-08-01 07:50:42 -0400

    add hidden property to drawer

--- bakery-client/bakery-web/src/scripts/drawer.ts
+++ bakery-client/bakery-web/src/scripts/drawer.ts
@@ -4,15 +4,23 @@ const overlay = document.getElementById('drawer-overlay') as HTMLElement;
 const pageWrapper = document.getElementById('page-wrapper') as HTMLElement;
 
 function openDrawer() {
-  drawer.classList.remove('-translate-x-full');
-  overlay.classList.remove('opacity-0', 'pointer-events-none');
-  overlay.classList.add('opacity-100', 'pointer-events-auto');
+  drawer.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+  setTimeout(() => {
+    drawer.classList.remove('-translate-x-full');
+    overlay.classList.remove('opacity-0', 'pointer-events-none');
+    overlay.classList.add('opacity-100', 'pointer-events-auto');
+  }, 100);
 }
 
 function closeDrawer() {
   drawer.classList.add('-translate-x-full');
   overlay.classList.add('opacity-0', 'pointer-events-none');
   overlay.classList.remove('opacity-100', 'pointer-events-auto');
+  setTimeout(() => {
+    drawer.classList.add('hidden');
+    overlay.classList.add('hidden');
+  }, 200);
 }
 
 drawerButton.addEventListener('click', (e: MouseEvent) => {
